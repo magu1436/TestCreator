627b6e18e108cfea140b810eb49cb65fcb6b1f6e
HodakaSakamoto <2cdi1130@cc.u-tokai.ac.jp>
2025-08-06 01:03:24 +0900

    単語帳モデル `WordList` を定義. `settings.py` に `wordbank` を登録. `WordList` モデルのマイグレーションを作成.

--- wordbank/models.py
+++ wordbank/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class WordList(models.Model):
+    """単語帳のモデル
+    
+    Fields:
+        name(CharField): 単語帳名
+        created_by(ForeignKey): 作成者（ユーザー）
+        created_at(DateTimeField): 作成日時
+    """
+    name = models.CharField("単語名", max_length=30)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name="作成者",
+        on_delete=models.PROTECT,
+        related_name="created_wordlists",   # userにcreated_wordlistsというメソッドを追加して、user側から一覧を取得できるようにする
+    )
+    created_at = models.DateTimeField("作成日時", auto_now_add=True)
+
+    def __str__(self):
+        return self.name
