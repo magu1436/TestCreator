41c6e0619ce823e69bafedb019f1655c4d615536
HodakaSakamoto <2cdi1130@cc.u-tokai.ac.jp>
2025-08-06 02:35:52 +0900

    `HomeView` と `home.html` のプロトタイプを作成. `home` アプリケーションに `urls.py` を作成し, `HomeView` を追加. また, `TestCreator` の `urls.py` に `home` を登録して, 最初に `HomeView` が 開かれるように設定.

--- TestCreator/settings.py
+++ TestCreator/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
     'accounts',
     'wordbank',
 ]
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja-JP'
 
 TIME_ZONE = 'Asia/Tokyo'
 
--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomeView(TemplateView):
+
+    def get(self, request):
+        return render(request, "home/home.html")
+    
+    def post(self, request):
+        return self.get(request)
