8472a5fd8f5ed862115706a270bcf8282ae2e073
HodakaSakamoto <2cdi1130@cc.u-tokai.ac.jp>
2025-08-07 05:37:27 +0900

    ログアウト機能を実装.

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 
 from django.contrib.auth.views import LoginView as BaseLoginView
+from django.contrib.auth.views import LogoutView as BaseLogoutView
+from django.urls import reverse_lazy
 
 from .forms import LoginForm
 
@@ -7,3 +9,7 @@ from .forms import LoginForm
 class LoginView(BaseLoginView):
     form_class = LoginForm
     template_name = "accounts/login.html"
+
+
+class LogoutView(BaseLogoutView):
+    next_page = "home"
