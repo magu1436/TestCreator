89967529c3a9fa252d34f280431c78db43369870
HodakaSakamoto <2cdi1130@cc.u-tokai.ac.jp>
2025-08-12 22:16:36 +0900

    単語編集画面のテーブルに単語を追加する関数群を追加. `単語手動追加` ボタンをクリックすると入力モーダルが開き, 入力して追加ボタンを 押すと, テーブルに単語が追加される. まだデータベースのやり取りは実装できていないため, 今後実装する必要あり.

--- vocab/static/vocab/js/edit.js
+++ vocab/static/vocab/js/edit.js
@@ -1,8 +1,13 @@
 
-const startRangeElement = document.getElementById("start-range")
-const endRangeElement = document.getElementById("end-range")
+const wordRowClassNames = ["word-row", "d-flex", "flex-row", "flex-shrink-0"];
+const wordRowChildrenClassNames = ["p-2", "border", "overflow-auto"];
 
-const searchBoxElement = document.getElementById("search_box")
+const startRangeElement = document.getElementById("start-range");
+const endRangeElement = document.getElementById("end-range");
+
+const searchBoxElement = document.getElementById("search_box");
+
+const wordTableContentElement = document.getElementById("word-table-content");
 
 
 /**
@@ -32,7 +37,7 @@ function searchWord(){
     for(let row of rows){
         for(let word of searchWords){
             const term = row.querySelector(".word-term").textContent.toLowerCase();
-            const meaning = row.querySelector(".word-meaning").textContent.toLowerCase()
+            const meaning = row.querySelector(".word-meaning").textContent.toLowerCase();
             if(!term.includes(word) && !meaning.includes(word)){
                 setVisible(row, false);
                 break;
@@ -57,10 +62,95 @@ function applyRangedNumberFilter(){
     if (startNum <= endNum){
         for(let row of rows){
             let wordNumber = Number(row.dataset.number);
-            setVisible(row, (startNum <= wordNumber && wordNumber <= endNum))
+            setVisible(row, (startNum <= wordNumber && wordNumber <= endNum));
         }
     }
 }
 
 startRangeElement.addEventListener("change", applyRangedNumberFilter);
 endRangeElement.addEventListener("change", applyRangedNumberFilter);
+
+
+/**
+ * 単語帳テーブルの行のHTML要素を作成して返す関数.
+ * 
+ * @param {Number} id 
+ * @param {String} number 単語番号
+ * @param {String} term 単語
+ * @param {String} meaning 意味
+ * @param {String} editor 最終更新者
+ * @returns {HTMLDivElement}
+ */
+function createWordRowElement(id, number, term, meaning, editor){
+    let wordRow = document.createElement("div");
+    wordRowClassNames.forEach(className => {
+        wordRow.classList.add(className);
+    });
+    wordRow.dataset.id = id;
+    wordRow.dataset.number = number;
+    
+    let wordNumber = document.createElement("div");
+    wordNumber.classList.add("word-number");
+    wordNumber.textContent = String(number);
+
+    let wordTerm = document.createElement("div");
+    wordTerm.classList.add("word-term");
+    wordTerm.textContent = term
+
+    let wordMeaning = document.createElement("div");
+    wordMeaning.classList.add("word-meaning");
+    wordMeaning.textContent = meaning;
+
+    let wordEditedBy = document.createElement("div");
+    wordEditedBy.classList.add("word-edited-by");
+    wordEditedBy.textContent = editor;
+
+    let rowChildren = [wordNumber, wordTerm, wordMeaning, wordEditedBy];
+    rowChildren.forEach(child => {
+        wordRowChildrenClassNames.forEach(className => {
+            child.classList.add(className);
+        });
+        wordRow.appendChild(child);
+    });
+
+    return wordRow
+}
+
+/**
+ * 単語帳のテーブルに単語を追加して表示する関数.  
+ * 追加する単語の番号を参照して, 昇順になるように挿入する.  
+ * 追加後は単語番号フィルターと単語検索を実行して, フィルターがある場合に追加されても  
+ * フィルターが機能するようにしている.  
+ * @param {Number} id 単語のデータベース上のID
+ * @param {String} number 単語番号
+ * @param {String} term 単語
+ * @param {String} meaning 意味
+ * @param {String} editor 最終更新者
+ */
+function insertWordRow(id, number, term, meaning, editor){
+    let wordRow = createWordRowElement(id, number, term, meaning, editor);
+    let is_insert = false;
+    for(let row of wordTableContentElement.getElementsByClassName("word-row")){
+        console.log(Number(row.dataset.number))
+        if(Number(number) < Number(row.dataset.number)){
+            row.before(wordRow);
+            is_insert = true;
+            break;
+        }
+    }
+    if(!is_insert) wordTableContentElement.appendChild(wordRow);
+
+    applyRangedNumberFilter();
+    searchWord();
+}
+
+/**
+ * 単語追加モーダルから値を取得してテーブルに追加する関数.  
+ * TODO: 取得した値をデータベースに登録し, IDと編集者を受け取ってそれらを登録する.
+ */
+function addWord(){
+    const number = document.getElementById("added-word-number").value;
+    const term = document.getElementById("added-word-term").value;
+    const meaning = document.getElementById("added-word-meaning").value;
+    insertWordRow(7, number, term, meaning, "test admin");
+}
