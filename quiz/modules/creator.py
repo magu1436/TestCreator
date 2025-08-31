806d8a5cdb7afee9683400b3b166563cf118285a
HodakaSakamoto <2cdi1130@cc.u-tokai.ac.jp>
2025-08-31 22:32:22 +0900

    作成したPDFにCSSを適用させた

--- quiz/modules/creator.py
+++ quiz/modules/creator.py
@@ -1,12 +1,15 @@
 
 import datetime
 from pathlib import Path
+from typing import Final
 
 from django.conf import settings
-from weasyprint import HTML
+from django.contrib.staticfiles import finders
+from django.utils import timezone
+from weasyprint import HTML, CSS
 
 
-PDF_NAME = str("test") + ".pdf"
+CSS_NAME: Final[str] = "quiz.css"
 
 def create_pdf(html: str):
     """HTMLをPDFに変換して保存する関数.  
@@ -14,9 +17,15 @@ def create_pdf(html: str):
     Args:
         html (str): PDFに変換するHTMLの文字列.
     """
+    print(finders.find("quiz/css/" + CSS_NAME))
     HTML(
         string=html, 
         base_url=settings.BASE_DIR
     ).write_pdf(
-        Path(settings.OUTPUT_PDF_DIR, PDF_NAME)
+        Path(settings.OUTPUT_PDF_DIR, __create_name()),
+        stylesheets=[CSS(filename=finders.find("quiz/css/" + CSS_NAME))],
     )
+
+def __create_name():
+    now = timezone.localtime(timezone.now())
+    return now.strftime("%Y%m%d_%H%M%S_%f.pdf")
