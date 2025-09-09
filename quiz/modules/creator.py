f6aa6fb4002c01a26c084953e490a69251f6fe2e
magu1436 <hfzoro77@gmail.com>
2025-09-09 17:10:43 +0900

    ２つのPDFをメモリに保存するとサーバーにメモリーエラーが生じて しまったため, 結合前に２つのファイルをPDFファイルとして保存し, 保存されたPDFファイルを参照して結合する実装に差し戻し.

--- quiz/modules/creator.py
+++ quiz/modules/creator.py
@@ -1,12 +1,15 @@
 
 from typing import Final
+from pathlib import Path
 
 from django.conf import settings
 from django.contrib.staticfiles import finders
+from django.utils import timezone
 from weasyprint import HTML, CSS
 
 
 CSS_NAME: Final[str] = "quiz.css"
+FILE_NAME_FORMAT: Final[str] = "%Y_%m_%d_%H_%M_%S_%f.pdf"
 
 def create_pdf(html: str):
     """HTMLをPDFに変換する関数.  
@@ -16,11 +19,17 @@ def create_pdf(html: str):
     Return:
         bytes: PDFのバイトデータ.
     """
-    pdf = HTML(
+    file_name = create_name()
+    HTML(
         string=html, 
         base_url=settings.BASE_DIR
     ).write_pdf(
+        target=Path(settings.OUTPUT_PDF_DIR, file_name),
         stylesheets=[CSS(filename=finders.find("quiz/css/" + CSS_NAME))],
     )
-    if (not(pdf)): raise ValueError("create pdf failed")
-    return pdf
+    return file_name
+
+
+def create_name():
+    now = timezone.localtime(timezone.now())
+    return now.strftime(FILE_NAME_FORMAT)
