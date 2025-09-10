1159ead5522b2571fcd199aaa99845e4d307c595
magu1436 <hfzoro77@gmail.com>
2025-09-10 12:00:59 +0900

    CloudRun側でメモリの問題が改善したため, 結合前のPDFをbytesで保持する方式に差し戻し
    This reverts commit f6aa6fb4002c01a26c084953e490a69251f6fe2e.

--- quiz/modules/creator.py
+++ quiz/modules/creator.py
@@ -1,15 +1,12 @@
 
 from typing import Final
-from pathlib import Path
 
 from django.conf import settings
 from django.contrib.staticfiles import finders
-from django.utils import timezone
 from weasyprint import HTML, CSS
 
 
 CSS_NAME: Final[str] = "quiz.css"
-FILE_NAME_FORMAT: Final[str] = "%Y_%m_%d_%H_%M_%S_%f.pdf"
 
 def create_pdf(html: str):
     """HTMLをPDFに変換する関数.  
@@ -19,17 +16,11 @@ def create_pdf(html: str):
     Return:
         bytes: PDFのバイトデータ.
     """
-    file_name = create_name()
-    HTML(
+    pdf = HTML(
         string=html, 
         base_url=settings.BASE_DIR
     ).write_pdf(
-        target=Path(settings.OUTPUT_PDF_DIR, file_name),
         stylesheets=[CSS(filename=finders.find("quiz/css/" + CSS_NAME))],
     )
-    return file_name
-
-
-def create_name():
-    now = timezone.localtime(timezone.now())
-    return now.strftime(FILE_NAME_FORMAT)
+    if (not(pdf)): raise ValueError("create pdf failed")
+    return pdf
